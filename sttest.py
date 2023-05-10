fd231ae3a859e8e7bd42d45c5bb6ab4535cda48d
Sana <sanatays01@gmail.com>
2023-05-10 10:56:30 +0200

    Déploiement via Heroku

--- sttest.py
+++ sttest.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 
 text = st.text_input(label = "Enter your question : ")
-res = requests.get("http://127.0.0.1:5000/api/text=" + ''.join(text))
+res = requests.get("https://app-api-imlp5.herokuapp.com/api/text=" + ''.join(text))
 res = pd.read_json(res.content.decode('utf-8')).loc[0, 'tags']
 
 st.write(res)
