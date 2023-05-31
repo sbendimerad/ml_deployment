54c73fb507d3b9d90d4e3c863bb6d37a6a39559e
Sana <sanatays01@gmail.com>
2023-05-31 12:58:46 +0200

    Auto stash before revert of "affichage output"

--- sttest.py
+++ sttest.py
@@ -7,6 +7,6 @@ import streamlit as st
 text = st.text_input(label = "Enter your question : ")
 res = requests.get("https://app-api-imlp5.herokuapp.com/api/text=" + ''.join(text))
 
-res = pd.read_json(res.content.decode('utf-8')).loc[0, 'tags']
+res = pd.read_json(res.content.decode('utf-8'), orient = 'records').loc[0, 'tags']
 
 st.write(res)
