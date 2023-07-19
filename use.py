f0d28315205bac832868383d4f2b7147268e49a6
Sabrine Bendimerad <sabrine.bendimerad1@gmail.com>
2023-07-19 17:54:28 +0200

    Update use.py

--- use.py
+++ use.py
@@ -1,72 +1,102 @@
-import nltk
-from string import punctuation
-import tensorflow_hub as hub
+import numpy as np
 
-# nltk.download('stopwords')
-# nltk.download('punkt')
-# nltk.download('averaged_perceptron_tagger')
-# nltk.download('wordnet')
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+from string import punctuation
+
+
+
 
 stop_words = nltk.corpus.stopwords.words("english")
-stop_words.extend(['what', 'how', 'where', 'who', 'which'])
-
-# Feature extraction using USE
-embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+for word in ['what', 'how', 'where', 'who', 'which'] :
+    stop_words.append(word)
 
 
-def preprocess(text):
-    # Load top 10 tags from file
-    with open("./top_10_tags.txt", "r") as file:
-        top_10_tags = file.read().split('\n')[:-1]
+def preprocess(text) :
 
+    file = open("./top20tags.txt", "r")
+    top20tags = file.read()
+    top20tags = list(top20tags.split('\n')[:-1])
+    file.close()
+
+    
+    # text convert to lowercase
     text = text.lower()
-
-    # Replace 'c#' with 'csharp'
-    for i in range(1, len(text)):
-        if text[i - 1] == 'c' and text[i] == '#':
-            text = text.replace(text[i], 'sharp')
-
+    
+    # remove punctuation in the string
+    text = text.translate(text.maketrans('', '', punctuation))  
+    
+    # Tokenization
     token_list = nltk.word_tokenize(text)
-
+    
     new_text = []
-
-    for token in token_list:
-        # Keep tokens that are top 10 tags
-        if token in top_10_tags:
+    
+    for token in token_list :
+        if token in top20tags :
             new_text.append(token)
-        # Remove stopwords and punctuation
-        elif token not in stop_words:
-            token = ''.join(char for char in token if char not in punctuation and not char.isdigit())
+        elif token not in stop_words :
+            for char in token :
+                if char.isdigit() :
+                    token = token.replace(char, '')
             new_text.append(token)
-
-
-    for token in new_text:
-        # Lemmatize verbs
-        if nltk.pos_tag([token])[0][1].startswith('V'):
-            index = new_text.index(token)
-            token_lem = lem.lemmatize(token, pos='v')
-            new_text[index] = new_text[index].replace(token, token_lem)
-
+    
+    # Lemmatisation
+    lem = nltk.stem.WordNetLemmatizer()
+    
+    # remove adjectives, adverbs, personal pronoun and preposition/subordinating conjunction
+    tags=['JJ','RB','PRP','IN','VB']
+    for token in new_text :
+        for tag in tags:
+            if nltk.pos_tag([token])[0][1].startswith(tag):
+                new_text.remove(token)
+    
+    # remove duplicate words
+    new_text=set(new_text)
+    new_text =[word for word in new_text if word!='']
+    print(new_text)
     new_text = ' '.join(new_text)
-
+    
+    # top20tags.txt follows the list in mlb.classes_
+    file = open("./top20tags.txt", "r")
+    top20tags = file.read()
+    top20tags = top20tags.split()[:20]
+    file.close()
+    vector=[0 for i in top20tags]
+    for word in new_text:
+        if word in top20tags:
+            vector[top20tags.index(word)]=1
     return new_text
+    #return tf.reshape(embed([new_text]), [-1]).numpy()
 
-def feature_USE_fct(sentences, b_size = 10) :
+
+def feature_USE_fct(sentences, b_size = 20) :
     
     batch_size = b_size
 
-    features = embed(sentences[0:batch_size]).numpy()
+    features = sentences[0:batch_size].numpy()
 
     return features
 
-def clean_output(result):
-    # Load top 10 tags from file
-    with open("./top_10_tags.txt", "r") as file:
-        top_10_tags = file.read().split('\n')[:-1]
 
-    output_tags_list = ['#' + top_10_tags[i] for i, tag in enumerate(result[0]) if tag == 1]
+def clean_output(result) :
+    # top20tags.txt follows the list in mlb.classes_
+    file = open("./top20tags.txt", "r")
+    top20tags = file.read()
+    top20tags = list(top20tags.split('\n')[:-1])
+    file.close()
 
-    if len(output_tags_list) == 0:
+    output_tags_list = []
+
+    for i in range(len(result[0])) :
+        if result[0][i] == 1 :
+          output_tags_list.append("#" + top20tags[i])
+
+    if len(output_tags_list) == 0 :
         output_tags_list = ["no recommendation..."]
 
     return output_tags_list
+
+
