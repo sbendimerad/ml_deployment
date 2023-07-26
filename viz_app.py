65cd1b2d5b9142cebb320e73c1d4d06804678c9e
BENDIMERAD <sabrine.bendimerad@saur.com>
2023-07-26 19:08:45 +0200

    add exception streamlit

--- viz_app.py
+++ viz_app.py
@@ -1,12 +1,16 @@
-import pandas as pd
 import requests
-
+import pandas as pd
 import streamlit as st
 
+text = st.text_input(label="Enter your question:")
 
-text = st.text_input(label = "Enter your question : ")
-res = requests.get("https://app-api-imlp5.herokuapp.com/api/text=" + ''.join(text))
+if text:
+    res = requests.get("http://127.0.0.1:5000/api/text=" + ''.join(text))
 
-res = pd.read_json(res.content.decode('utf-8'), orient = 'records').loc[0, 'tags']
-
-st.write(res)
+    if res.status_code == 200:
+        res = pd.read_json(res.content.decode('utf-8'), orient='records').loc[0, 'tags']
+        st.write(res)
+    else:
+        st.write("Error: Unable to fetch data from the API.")
+else:
+    st.write("Please enter a question.")
